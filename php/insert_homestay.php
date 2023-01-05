0ef1549062f77ad7b984c25f8726560c644190af
Wong Re Jie <72849000+wongrejie@users.noreply.github.com>
2023-01-06 01:11:08 +0800

    Update insert_homestay.php

--- php/insert_homestay.php
+++ php/insert_homestay.php
@@ -14,15 +14,24 @@
 	$local= addslashes($_POST['local']);
 	$lat= $_POST['lat'];
 	$lon= $_POST['lon'];
-	$image= $_POST['image'];
+	$image1= $_POST['image1'];
+	$image2= $_POST['image2'];
+	$image3= $_POST['image3'];
 	
 	$sqlinsert = "INSERT INTO `table_homestay`(`user_id`, `homestay_name`, `homestay_desc`, `homestay_address`, `homestay_price`, `homestay_state`, `homestay_local`, `homestay_lat`, `homestay_lng`) VALUES ('$userid','$hsname','$hsdesc','$hsaddress','$hsprice','$state','$local','$lat','$lon')";
   try {
 		if ($conn->query($sqlinsert) === TRUE) {
-			$decoded_string = base64_decode($image);
+			$decoded_string1 = base64_decode($image1);
+			$decoded_string2 = base64_decode($image2);
+			$decoded_string3 = base64_decode($image3);
 			$filename = mysqli_insert_id($conn);
-			$path = '../assets/homestayImages/'.$filename.'.png';
-			file_put_contents($path, $decoded_string);
+			$path1 = '../assets/homestayimages/'.$filename.'.1.png';
+			$path2 = '../assets/homestayimages/'.$filename.'.2.png';
+			$path3 = '../assets/homestayimages/'.$filename.'.3.png';
+			file_put_contents($path1, $decoded_string1);
+			file_put_contents($path2, $decoded_string2);
+			file_put_contents($path3, $decoded_string3);
+			
 			$response = array('status' => 'success', 'data' => null);
 			sendJsonResponse($response);
 		}
